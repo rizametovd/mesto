497582a4abeaa369dc05504430621bf1dc70c5b3
David <rizametov@gmail.com>
2020-12-07 15:54:58 +0300

    5 проект внес исправления после ревью

--- scripts/script.js
+++ scripts/script.js
@@ -15,79 +15,6 @@ const cardTemplate = document.querySelector('.card__template');
 const popupAdd = document.querySelector('.popup-add');
 const popupImg = document.querySelector('.popup-img');
 const popupEdit = document.querySelector('.popup-edit');
-
-
-// Функция Открыть попап
-function openPopup(popup) {
-    popup.classList.add('popup_opened');
-}
-
-// Слушатель на кнопке Добавить
-addButton.addEventListener('click', function (e) {
-    openPopup(popupAdd);
-})
-
-// Слушатель на кнопке Редактировать
-editButton.addEventListener('click', function (e) {
-    popupNameField.value = profileName.textContent;
-    popupAboutField.value = profileAbout.textContent;
-    openPopup(popupEdit);
-})
-
-
-// Функция закрыть попап
-function closePopup(popup) {
-    popup.classList.remove('popup_opened');
-}
-
-// Слушатель на кнопке закрыть попап
-closeAddButton.addEventListener('click', function (e) {
-    closePopup(popupAdd);
-});
-
-// Слушатель на кнопке закрыть попап
-closeEditButton.addEventListener('click', function (e) {
-    closePopup(popupEdit);
-});
-
-// Слушатель на кнопке закрыть попап
-closeImagePopup.addEventListener('click', function (e) {
-    closePopup(popupImg);
-})
-
-
-// Добавить карточку и закрыть попап
-function addImage(evt) {
-    evt.preventDefault();
-
-    const imageName = document.querySelector('.popup__image-name');
-    const imageLink = document.querySelector('.popup__image-link');
-
-    item = {
-        name: imageName.value,
-        link: imageLink.value
-    }
-
-    addCard(cardsList, createCard(item))
-    closePopup(popupAdd);
-    addForm.reset();
-}
-addForm.addEventListener('submit', addImage);
-
-
-//Изменить профиль и закрыть попап
-function formSubmitHandler(evt) {
-    evt.preventDefault();
-
-    profileName.textContent = popupNameField.value;
-    profileAbout.textContent = popupAboutField.value;
-
-    closePopup(popupEdit);
-}
-editForm.addEventListener('submit', formSubmitHandler);
-
-
-// Загрузка 6 карточек из массива
 const initialCards = [
     {
         name: 'Архыз',
@@ -116,6 +43,43 @@ const initialCards = [
 ];
 
 
+// Функция Открыть попап
+function openPopup(popup) {
+    popup.classList.add('popup_opened');
+}
+
+// Функция закрыть попап
+function closePopup(popup) {
+    popup.classList.remove('popup_opened');
+}
+
+// Функция добавления карточки в форме Добавить
+function addImage(evt) {
+    evt.preventDefault();
+
+    const imageName = document.querySelector('.popup__image-name');
+    const imageLink = document.querySelector('.popup__image-link');
+
+    const item = {
+        name: imageName.value,
+        link: imageLink.value
+    }
+
+    addCard(cardsList, createCard(item))
+    closePopup(popupAdd);
+    addForm.reset();
+}
+
+// Функция, которая присвает профиль в инпуты
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+
+    profileName.textContent = popupNameField.value;
+    profileAbout.textContent = popupAboutField.value;
+
+    closePopup(popupEdit);
+}
+
 // Функция создания карточки
 function createCard(item) {
     const newCard = cardTemplate.content.cloneNode(true);
@@ -130,7 +94,7 @@ function createCard(item) {
 
     likeButton.addEventListener('click', like);
     removeCardButton.addEventListener('click', removeCard);
-    cardImage.addEventListener('click', () => OpenImagePopup(item));
+    cardImage.addEventListener('click', () => openImagePopup(item));
 
     return newCard;
 
@@ -142,7 +106,7 @@ function addCard(location, cardElement) {
 }
 
 // Открытие попапа с картинкой
-function OpenImagePopup(item) {
+function openImagePopup(item) {
     const popupImage = document.querySelector('.popup__image');
     const popupImageCaption = document.querySelector('.popup__image-caption');
     popupImage.src = item.link;
@@ -166,4 +130,42 @@ function renderCards() {
     const cards = initialCards.map(createCard);
     cardsList.append(...cards);
 }
+
+
+// Слушатели
+
+// Слушатель на кнопке Добавить
+addButton.addEventListener('click', function (e) {
+    openPopup(popupAdd);
+})
+
+// Слушатель на кнопке Редактировать
+editButton.addEventListener('click', function (e) {
+    popupNameField.value = profileName.textContent;
+    popupAboutField.value = profileAbout.textContent;
+    openPopup(popupEdit);
+})
+
+// Слушатель на кнопке закрыть попап
+closeAddButton.addEventListener('click', function (e) {
+    closePopup(popupAdd);
+});
+
+// Слушатель на кнопке закрыть попап
+closeEditButton.addEventListener('click', function (e) {
+    closePopup(popupEdit);
+});
+
+// Слушатель на кнопке закрыть попап
+closeImagePopup.addEventListener('click', function (e) {
+    closePopup(popupImg);
+})
+
+// Слушатель рендинга новой карточки
+addForm.addEventListener('submit', addImage);
+
+// Слушатель формы Редактировать профиль
+editForm.addEventListener('submit', formSubmitHandler);
+
+// Вызов функции рендинга 6 карточке
 renderCards()
